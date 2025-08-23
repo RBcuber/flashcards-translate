066a414c0d2f72c5396be57bb1faa546d06541ed
RBcuber <igor.kirichenko.07@gmail.com>
2025-08-23 22:47:28 +0200

    fix: bug in localhost

--- public/js/translator.js
+++ public/js/translator.js
@@ -1,5 +1,13 @@
 export async function translateRuToEn(text) {
-  const url = `https://flashcards-translate.vercel.app/api/translate?text=${encodeURIComponent(text)}&source=ru&target=en`;
+  const baseUrl =
+    window.location.hostname === "localhost"
+      ? "http://localhost:3000"
+      : "https://flashcards-translate.vercel.app";
+
+  const url = `${baseUrl}/api/translate?text=${encodeURIComponent(
+    text
+  )}&source=ru&target=en`;
+
   const res = await fetch(url);
   const data = await res.json();
   return data.data?.translations?.[0]?.translatedText ?? "";
