27481cbb2ba73d40c7d460e177dfd40465f68b38
RBcuber <igor.kirichenko.07@gmail.com>
2025-08-23 22:26:22 +0200

    add: translate

--- public/js/translator.js
+++ public/js/translator.js
@@ -1,7 +1,6 @@
 export async function translateRuToEn(text) {
-  const q = encodeURIComponent(text);
-  const url = `https://api.mymemory.translated.net/get?q=${q}&langpair=ru%7Cen`;
+  const url = `https://flashcards-translate.vercel.app/api/translate?text=${encodeURIComponent(text)}&source=ru&target=en`;
   const res = await fetch(url);
   const data = await res.json();
-  return data.responseData.translatedText ?? "";
+  return data.data?.translations?.[0]?.translatedText ?? "";
 }
